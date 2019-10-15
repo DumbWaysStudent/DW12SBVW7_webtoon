d6420b262bd33428dfdff1cf22b5020cb0113a65
Arief Rachman <siccmeown@gmail.com>
2019-10-16 02:48:41 +0700

    add delete image implementation

--- controllers/page.js
+++ controllers/page.js
@@ -35,9 +35,10 @@ exports.createPage = async (req, res) => {
     if (req.authorize_user.id == req.params.userId) {
       const page = {
         page: req.body.page,
-        image: 'https://swebtoon-phinf.pstatic.net/20140617_248/1403004901360ABk5x_JPEG/tower_000.jpg',
-        episode_id: req.params.episodeId
-      }
+        image:
+          'https://swebtoon-phinf.pstatic.net/20140617_248/1403004901360ABk5x_JPEG/tower_000.jpg',
+        episode_id: req.params.episodeId,
+      };
       const data = await Page.create(page);
       res.json(data);
     }
@@ -46,3 +47,19 @@ exports.createPage = async (req, res) => {
   }
 };
 
+exports.deletePage = async (req, res) => {
+  try {
+    if (req.authorize_user.id == req.params.userId) {
+      await Page.destroy({
+        where: { episode_id: req.params.episodeId, id: req.params.im },
+      });
+      res.json({ id: req.params.imageId });
+    } else {
+      res
+        .status(401)
+        .json({ auth: 'You dont have permission to access this route!' });
+    }
+  } catch (error) {
+    res.status(500).json(error);
+  }
+};
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const authorization = require('../middlewares/authorized').authorization;
 
 const { findAllUserToon, createToon, updateUserToon, deleteUserToon } = require('../controllers/user');
 const { findAllUserEpisode, createEpisode, updateEpisode, deleteEpisode } = require('../controllers/episode');
-const { findAllUserPages, createPage } = require('../controllers/page');
+const { findAllUserPages, createPage, deletePage } = require('../controllers/page');
 
 routes.use(authorization);
 
@@ -22,4 +22,6 @@ routes.delete('/:userId/sanstoon/:sanstoonId/episode/:episodeId', deleteEpisode)
 // Page
 routes.get('/:userId/sanstoon/:sanstoonId/episode/:episodeId/images', findAllUserPages);
 routes.post('/:userId/sanstoon/:sanstoonId/episode/:episodeId/image', createPage);
+routes.delete('/:userId/sanstoon/:sanstoonId/episode/:episodeId/image/:imageId', deletePage);
+
 module.exports = routes;
